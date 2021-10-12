8cafaee859489c652c839bb0baf47697848698a5
Bruno <cocacola_bruno@hotmail.com>
2021-10-12 13:51:24 -0300

    1 - Refatorando - Dev

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,10 +1,20 @@
 
+function calc(value, percentage){
+    return value * (1 - percentage);
+}
+
 function calcular(funcionario){
-    if(funcionario.cargo === 'DESENVOLVEDOR'){
-        if(funcionario.salarioBase >= 3000){
-            return funcionario.salarioBase * (1 - 0.20);
+
+    switch (funcionario.cargo){
+        case "DESENVOLVEDOR":{
+            if(funcionario.salarioBase >= 3000){
+                return calc(funcionario.salarioBase, 0.20)
+            }else {
+                return calc(funcionario.salarioBase, 0.10)
+            }
         }
     }
+
 }
 
 exports.calcular = calcular;
