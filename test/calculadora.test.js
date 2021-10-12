78220b3d81ba3ada6ee393643c6878d42821f72a
Bruno <cocacola_bruno@hotmail.com>
2021-10-12 13:55:19 -0300

    2 - Test Fail - DBA

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -28,4 +28,30 @@ describe("Calculadora de Salarios", () => {
 
     })
 
+    test("Calcula DBA - 2800", () => {
+
+        let funcionario = {
+            nome: "fulano",
+            email: "fulano@email.com",
+            salarioBase: 2800,
+            cargo: "DBA"
+        }
+
+        expect(calcular(funcionario)).toBe(2100)
+
+    })
+
+    test("Calcula DBA - 1900", () => {
+
+        let funcionario = {
+            nome: "fulano",
+            email: "fulano@email.com",
+            salarioBase: 1900,
+            cargo: "DBA"
+        }
+
+        expect(calcular(funcionario)).toBe(1615)
+
+    })
+
 })
