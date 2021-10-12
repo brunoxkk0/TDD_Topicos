1723e8adec5735e5c31bb21e6121a751611cf6a8
Bruno <cocacola_bruno@hotmail.com>
2021-10-12 14:00:38 -0300

    2 - Test Pass - DBA

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,12 +6,25 @@ function calc(value, percentage){
 function calcular(funcionario){
 
     switch (funcionario.cargo){
+
         case "DESENVOLVEDOR":{
+
             if(funcionario.salarioBase >= 3000){
                 return calc(funcionario.salarioBase, 0.20)
             }else {
                 return calc(funcionario.salarioBase, 0.10)
             }
+
+        }
+
+        case "DBA":{
+
+            if(funcionario.salarioBase >= 2000){
+                return funcionario.salarioBase * (1 - 0.25)
+            }else{
+                return funcionario.salarioBase * (1 - 0.15)
+            }
+
         }
     }
 
